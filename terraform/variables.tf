667be558b8291b3ebb5f7e98c91bf020a11c78b6
ravishp510 <60631017+ravishp510@users.noreply.github.com>
2025-07-31 14:35:21 -0400

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "ec2_name" {
+  type = string
+}
+
+variable "pem_key_name" {
+  type = string
+}
+
+variable "aws_region" {
+  type = string
+}
+
+variable "ec2_instance_type" {
+  type = string
+}
